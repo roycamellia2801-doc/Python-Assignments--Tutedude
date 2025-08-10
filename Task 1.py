4f7c2c6b22d14c8448bc0d78b0aad9b153fdc55a
Dr. Camellia Roy <roycamellia2801@gmail.com>
2025-08-10 22:48:47 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,13 @@
+#Step 1: Get input from user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# Step 2: Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print(f"\nResults of operations on {num1} and {num2}:")
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
